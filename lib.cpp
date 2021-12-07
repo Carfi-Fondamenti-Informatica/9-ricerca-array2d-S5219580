bf566bdf0ed9cd9ac1027a4cf406854a0ded6505
S5219580 <91791844+S5219580@users.noreply.github.com>
2021-12-07 15:02:11 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
-
+#include<iostream>
+#include'lib.h
+using namespace std;
+void verifica(char array[10][20]){
+char nome[20];
+cin>>nome;
+for(int i=0; i<10;i++){
+    if (strcmp(nome,array[i])==0){
+        cout<<i+1;
+        return;}}
+cout<<"non presente";
+return;
+}
+void inserisci(char array[10][20]){
+    for(int i=0; i<10;i++){
+    cin>>array[i];}
+return;}
